940312a01af38ed8d3025bfd9ed99f7649be0a95
ahgalib <asadullahalgalib783@gmail.com>
2021-07-01 16:37:46 +0600

    admin confirm product for sell

--- admin/inbox.php
+++ admin/inbox.php
@@ -1,63 +1,71 @@
 ﻿<?php include 'inc/header.php';?>
 <?php include 'inc/sidebar.php';?>
-        <div class="grid_10">
-            <div class="box round first grid">
-                <h2>Inbox</h2>
-                <div class="block">        
-                    <table class="data display datatable" id="example">
-					<thead>
-						<tr>
-							<th>Serial No.</th>
-							<th>Message</th>
-							<th>Action</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr class="odd gradeX">
-							<td>01</td>
-							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="even gradeC">
-							<td>02</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="odd gradeX">
-							<td>03</td>
-							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="even gradeC">
-							<td>04</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-							<tr class="odd gradeX">
-							<td>05</td>
-							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="even gradeC">
-							<td>06</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="odd gradeX">
-							<td>07</td>
-							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="even gradeC">
-							<td>08</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-					</tbody>
-				</table>
-               </div>
-            </div>
-        </div>
+<?php 
+include_once '../classes/cartclass.php';
+$obj_cart = new cart();
+ ?>
+<div class="grid_10">
+    <div class="box round first grid">
+        <h2>Inbox</h2>
+        <div class="block">        
+            <table class="data display datatable" id="example">
+			<thead>
+				<tr>
+					<th>Serial No.</th>
+					<th>Product</th>
+					<th>Customer ID</th>
+					<th>Quantity</th>
+					<th>Price</th>
+					<th>Image</th>
+					<th>Date</th>
+					<th>Action</th>
+				</tr>
+			</thead>
+			<tbody>
+			<?php 
+			//update status
+			if(isset($_GET['cid'])){
+				$cid = $_GET['cid'];
+				$oderid = $_GET['orid'];
+				$shift = $obj_cart->upsta($cid,$oderid);
+
+			}
+			if(isset($shift)){
+				echo $shift;
+			}
+			 ?>
+			<?php 
+			$selc = $obj_cart->showOrderadmin();
+				if($selc){
+					$i = 0;
+				while($row = $selc->fetch_assoc()){
+						$i++;
+				 ?>
+				<tr class="odd gradeX">
+					<td><?php echo $i; ?></td>
+					<td><?php echo $row['pro_name']; ?></td>
+					<td><?php echo $row['cus_id']; ?></td>
+					<td><?php echo $row['quantity']; ?></td>
+					<td><?php echo $row['price']; ?></td>
+					<td><img style="width:70px;height:40px;"src="<?php echo $row['image']; ?>"></td>
+					<td><?php echo $row['dateee']; ?></td>
+					<td>
+						<?php 
+						if($row['status'] == 0){
+						 ?>
+							<a href="?cid=<?php echo $row['cus_id']; ?>& orid=<?php echo $row['or_id']; ?>">click to sell</a>
+						<?php }else{
+							echo "soild";
+						} ?>
+					</td>
+				</tr>
+			<?php }} ?>
+
+			</tbody>
+		</table>
+       </div>
+    </div>
+</div>
 <script type="text/javascript">
     $(document).ready(function () {
         setupLeftMenu();
